a1ac960ff185f6bf21119cdf2f3477aa761f5a54
GuiSouza <guilhermespublic@gmail.com>
2019-03-19 10:21:39 -0300

    PageHeader adicionado e inclusão de infos About

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import PageHeader from '../template/pageHeader'
 
 export default props => (
     <div>
-        <h1>Sobre</h1>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+
+        <h2>Nossa História</h2>
+        <p>Vai virar cinema</p>
+        <h2>Missão e Visão</h2>
+        <p>E faltou os valores</p>
+        <h2>Imprensa</h2>
+        <p>Ou pode ser na chapa mesmo</p>
     </div>
 )
